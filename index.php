3e1c1aff5b0c65b98fd520163a2129f73fdfa18b
Tran Anh Toan <trananhtoan2506@gmail.com>
2025-09-11 14:28:45 +0700

    Tạo thêm file

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
-<?php
-
-?>
-
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        getStart();
-    ?>
+
 </body>
 </html>
