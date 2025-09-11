94da358fdeef9a6be0914e286f198579eb0cd268
Tran Anh Toan <trananhtoan2506@gmail.com>
2025-09-11 13:20:38 +0700

    du lieu moi

---
+++ dev/check_login.php
@@ -0,0 +1,19 @@
+<?php
+    function check_login($username, $password) {
+        $error = [];
+        $username = htmlspecialchars(trim($username));
+        $password = trim($password);
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            // kiểm tra tên đăng nhập và mật khẩu và email
+            if(empty($_POST["username"]) || empty($_POST["password"])) {
+                error_log("Vui lòng kiểm tra lại tên đăng nhập và mật khẩu!!!");
+                return $error;
+            }
+            else{
+                return[
+                    'data' => ['username' => $username, 'password' => $password],
+                ];
+            }
+        }
+    }
